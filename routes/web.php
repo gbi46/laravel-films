2813c0dd99311d416faffea53b19109350df8478
ivcoder <vank300828@gmail.com>
2025-02-17 15:19:02 +0100

    add api routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use App\Http\Controllers\GenreController;
+use App\Http\Controllers\MovieController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/apis', function() {
+    return view('apis');
+});
+
+Route::get('genres', [GenreController::class, 'index']);
+Route::get('genres/{id}', [GenreController::class, 'show']);
+
+Route::get('movies', [MovieController::class, 'index']);
+Route::get('movies/{id}', [MovieController::class, 'show']);
+Route::post('movies', [MovieController::class, 'store']);
+Route::patch('movies/{id}', [MovieController::class, 'update']);
+Route::patch('movies/{id}/publish', [MovieController::class, 'publish']);
